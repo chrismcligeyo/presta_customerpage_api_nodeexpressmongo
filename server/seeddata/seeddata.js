dfab0cd9fdb051479df93bf18c5230efbd934768
= <mcligeyochristopher@gmail.com>
2023-06-05 21:56:32 +0300

    added seed data

---
+++ server/seeddata/seeddata.js
@@ -0,0 +1,277 @@
+[
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074u"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074t"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074s"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074r"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074q"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074p"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074o"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074n"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074m"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074l"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074k"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074j"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074i"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074h"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074g"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074f"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074e"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074d"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074c"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074b"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074u"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074v"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074w"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074x"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+    {
+        "id":{
+            "$oid": "61fbaf25671c45f3f5f4074y"
+        },
+        "name": "john kamau",
+        "phone_number": "+254794830933",
+        "branch": "HQ",
+        "sales_rep":"ryan muraya",
+        "approved": "yes",
+        "loan": 23123123
+    },
+]
